642d40e5a4109f4b287e79aa3dfe453924b02455
liamcraddock1996 <c1381084@student.southstaffs.ac.uk>
2017-06-22 13:03:50 +0100

    updated main.js

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ preload: function() {
 //Load the bird sprite
 game.load.image('bird', 'assets/bird.png');
 game.load.image('pipe', 'assets/pipe.png');
+game.load.audio('jump', 'assets/jump.wav');
 },
     
 create: function() {
@@ -17,7 +18,7 @@ create: function() {
 // Here we set up the game, display sprites, etc.
 //Change the background colour of the game to blue - for now!
     
-game.stage.backgroundColor = '#71c5cf';
+game.stage.backgroundColor = '#71c5cf'; 
     
 //Set the physics system
 game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -31,7 +32,9 @@ game.physics.arcade.enable(this.bird);
     
 //Add gravity to the bird to make it fall
 this.bird.body.gravity.y = 1000;
-    
+// New anchor position
+
+this.bird.anchor.setTo(-0.2, 0.5);    
 //Call the 'jump' function when the spacebar is pressed
 var spaceBar = game.input.keyboard.addKey(
 Phaser.Keyboard.SPACEBAR);
@@ -40,9 +43,13 @@ spaceBar.onDown.add(this.jump, this);
 //Score
 this.score = 0;
 this.labelScore = game.add.text(20, 20, "0",
+                                
 
 { font: "30px Arial", fill: "#ffffff" });
 },
+    
+//Add the sound to the game
+this.jumpSound = game.add.audio('jump');
 
 //Timer for pipes
 this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
@@ -56,17 +63,57 @@ update: function() {
 //If the 'bird' is out of the screen (too high or too low)
 //Call the 'restartGame' function
 if (this.bird.y < 0 || this.bird.y > 490)
-this.restartGame();
+this.hitpipe();
+//changed from this.restartgame to this.hitpipe    
+    
+// Slowly rotate the bird downward, up to a certain point
+if (this.bird.angle < 20)
+this.bird.angle += 1;
 },
 
 //Make the bird jump
 jump: function() {
+
+//Stops the bird being controlled when it is dead. Dead means dead.
+if (this.bird.alive == false)
+return;
+    
 //Add a vertial velocity to the bird
 this.bird.body.velocity.y = -350;
+    
+//Simplified code for upward flying animation
+
+game.add.tween(this.bird).to({angle: -20}, 100).start();
+},
+    
+hitpipe: function() {
+
+//If the bird has already hit a pipe, do nothing
+//It means the bird is already falling off the screen
+
+if (this.bird.alive == false)
+return;
+
+//Set the alive property of the bird to false
+this.bird.alive = false;
+
+//Prevent new pipes from appearing
+game.time.events.remove(this.timer);
+
+//Go through all the pipes, and stop their movement
+this.pipes.forEach(function(p){
+
+p.body.velocity.x = 0;
+}, this);
+
+// Play sound
+
+this.jumpSound.play();
 },
 
+    
 //Restart the game
-restartGame: function() {
+hitpipe: function() {
 //Start the 'main' state, which restarts the game
 game.state.start('main');
     
@@ -74,6 +121,7 @@ game.state.start('main');
 game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame,
 null, this);
 },
+
     
 //Adds a pipe
 addOnePipe: function(x, y) {
