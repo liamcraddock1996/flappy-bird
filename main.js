52f74e6c6466491f709455a5a5deaf45a16f0c5f
liamcraddock1996 <c1381084@student.southstaffs.ac.uk>
2017-06-22 11:50:02 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -9,8 +9,9 @@ preload: function() {
 preload: function() {
 //Load the bird sprite
 game.load.image('bird', 'assets/bird.png');
+game.load.image('pipe', 'assets/pipe.png');
 },
-
+    
 create: function() {
 // This function is called after the preload function
 // Here we set up the game, display sprites, etc.
@@ -37,6 +38,12 @@ Phaser.Keyboard.SPACEBAR);
 spaceBar.onDown.add(this.jump, this);
 },
 
+//Timer for pipes
+this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+    
+//Create an empty group
+this.pipes = game.add.group();
+
 update: function() {
 // This function is called 60 times per second
 // It contains the game's logic
@@ -56,6 +63,40 @@ this.bird.body.velocity.y = -350;
 restartGame: function() {
 //Start the 'main' state, which restarts the game
 game.state.start('main');
+},
+
+//Adds a pipe
+addOnePipe: function(x, y) {
+
+//Adds 6 pipes
+addRowOfPipes: function() {
+
+//Randomly pick a number between 1 and 5
+//This will be the hole position in the pipe for the bird to fly through
+var hole = Math.floor(Math.random() * 5) + 1;
+
+//Add the 6 pipes
+//With one big hole at position 'hole' and 'hole + 1'
+for (var i = 0; i < 8; i++)
+if (i != hole && i != hole + 1)
+this.addOnePipe(400, i * 60 + 10);
+},
+
+//Create a pipe at the position x and y
+var pipe = game.add.sprite(x, y, 'pipe');
+
+//Add the pipe to our previously created group
+this.pipes.add(pipe);
+
+//Enable physics on the pipe
+game.physics.arcade.enable(pipe);
+
+//Add velocity to the pipe to make it move left
+pipe.body.velocity.x = -200;
+
+//Automatically kill the pipe when it is no longer visible
+pipe.checkWorldBounds = true;
+pipe.outOfBoundsKill = true;
 },
 
 };
