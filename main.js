d2b19c63ba6a53c75a1f9ac731097ee72274c323
liamcraddock1996 <c1381084@student.southstaffs.ac.uk>
2017-06-27 12:53:46 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -144,3 +144,10 @@ var mainState = {
 
 // Initialize Phaser, and create a 400px by 490px game
 var game = new Phaser.Game(400, 490);
+
+// Add the 'mainState' and call it 'main'
+game.state.add('main', mainState);
+
+// Start the state to actually start the game
+game.state.start('main');
+
